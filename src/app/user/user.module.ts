354877b5fdd4209aff90a0ecb894bc873ce84755
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2021-06-21 10:04:15 +0200

    Add user components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
