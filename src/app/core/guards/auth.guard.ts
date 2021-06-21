5c01937757f32e4e41816c14ee1912499ced03e2
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2021-06-21 12:39:40 +0200

    Added auth guard to restrict access to home

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { UserService } from './../services/user.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  public constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      this.userService.authenticated()
+        .subscribe((state: boolean) => {
+          this.router.navigate(['user','login']);
+        });
+      return this.userService.authenticated();
+  }
+
+}
