1cf244e29017715f6713adb583a6f2d2dbeca48c
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2021-06-23 08:58:33 +0200

    Add form for login

--- src/app/user/pages/login/login.component.ts
+++ src/app/user/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      login: [
+        '',
+        Validators.required
+      ],
+      password: [
+        '',
+        Validators.required
+      ]
+    });
   }
 
 }
